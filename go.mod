182625143f4c4d364b6a3e535c8052c8533994f1
AJ-Brown-InTech <ajalantbrown@gmail.com>
2022-12-16 11:07:33 -0500

    go server importr link

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/AJ-Brown-InTech/cmd
+module github.com/AJ-Brown-InTech/libre-api
 
-go 1.19
+go 1.18
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/AJ-Brown-InTech/libre-api/server"
+)
 
 func main (){
 	fmt.Println("hello world")
+	server.Server()
 }
