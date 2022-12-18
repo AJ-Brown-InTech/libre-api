cf293f946442339bff9c1c240b247b25e50c2fe9
AJ-Brown-InTech <ajalantbrown@gmail.com>
2022-12-17 20:20:03 -0500

    configs for api anf db

--- config/config.go
+++ config/config.go
@@ -1,22 +1,21 @@
 package config
 
 import (
-	"fmt"
+	"log"
 	"time"
+	"errors"
+	"github.com/spf13/viper"
 
 )
 
-// App config struct
+// Api config struct
 type Config struct {
 	Server   ServerConfig
 	Postgres PostgresConfig
 	MongoDB  MongoDB
-	Cookie   Cookie
 	Session  Session
-	Metrics  Metrics
 	Logger   Logger
 }
-
 
 // Server config struct
 type ServerConfig struct {
@@ -25,7 +24,6 @@ type ServerConfig struct {
 	PprofPort         string
 	Mode              string
 	JwtSecretKey      string
-	CookieName        string
 	ReadTimeout       time.Duration
 	WriteTimeout      time.Duration
 	SSL               bool
@@ -59,19 +57,6 @@ type MongoDB struct {
 	MongoURI string
 }
 
-type Metrics struct {
-	URL         string
-	ServiceName string
-}
-
-// Cookie config
-type Cookie struct {
-	Name     string
-	MaxAge   int
-	Secure   bool
-	HTTPOnly bool
-}
-
 
 // Session config
 type Session struct {
@@ -80,6 +65,29 @@ type Session struct {
 	Expire int
 }
 
-func Configuration (){
-	fmt.Println("config here")
+// Load config file from given path
+func LoadConfig(filename string) (*viper.Viper, error) {
+	v := viper.New()
+	v.SetConfigName(filename)
+	v.AddConfigPath(".")
+	v.AutomaticEnv()
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, errors.New("config file not found")
+		}
+		return nil, err
+	}
+
+	return v, nil
+}
+
+// Parse config file
+func ParseConfig(v *viper.Viper) (*Config, error) {
+	var c Config
+	err := v.Unmarshal(&c)
+	if err != nil {
+		log.Printf("unable to decode into struct, %v", err)
+		return nil, err
+	}
+	return &c, nil
 }
